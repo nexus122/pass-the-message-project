cc60f1e6cb280a19527fe7462b4b6653d88f576d
nexus122 <juan-pablo122@hotmail.es>
2022-04-06 13:18:41 +0200

    Vue Pass The Message

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 // Tu código aquí
 
 
-form.addEventListener('submit', function(e){
-    console.log("Hemos hecho clic en el botón de submit!")
-
-})
+Vue.createApp({
+    data(){
+        return{
+            message: '',
+            downMessage: '',
+            alert: 'You must write a message'
+        }
+    },
+    methods:{
+        passTheMessage(){            
+            this.downMessage = this.message.trim() != '' ? this.message : ''
+            this.alert = this.message.trim() == '' ? 'A blank space is not a message...': 'You must write a message'
+            this.message = ''
+        }
+    }
+}).mount('#app');
